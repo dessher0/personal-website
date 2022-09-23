36aa5af4751634c0ef4b0066ee9c765e5c1892cb
Maciej <m.kula@netperformers.de>
2022-09-23 21:24:26 +0200

    Implemented global variables

--- app.py
+++ app.py
@@ -6,15 +6,20 @@ import config
 def page_not_found(e):
   return render_template('parts/error_404.html'), 404
 
-
 app = Flask(__name__)
 app.config.from_object(config.config['development'])
 
 app.register_error_handler(404, page_not_found)
 
+@app.context_processor
+def process():
+    currentYear = config.CURRENT_YEAR
+    
+    return dict(currentYear=currentYear)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', **locals())
 
 @app.route('/openai', methods=["GET", "POST"])
 def openai():
@@ -25,8 +30,8 @@ def openai():
             response = response[2:]
             Result = response.replace('?', '')
 
-    return render_template('openai.html', **locals())
 
+    return render_template('openai.html', **locals())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8888', debug=True)
