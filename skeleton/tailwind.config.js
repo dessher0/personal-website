6571a68dac7a17c25e738a38ebe3b2e9edfb14fe
Maciej <m.kula@netperformers.de>
2022-10-01 18:01:29 +0200

    Added custom theme & rebuild CSS

--- skeleton/tailwind.config.js
+++ skeleton/tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./templates/**/*.html",
-    "./static/src/**/*.js"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				custom: {
+					"primary": '#facc15',
+					"secondary": '#fb7185',
+					"accent": '#a78bfa',
+					"neutral": '#3D4451',
+					"base-100" : '#FFFFFF',
+					"info": '#3ABFF8',
+					"success": '#36D399',
+					"warning": '#fbbf24',
+					"error": '#f87171',
+				},
+			},
+		],
+	},
+	content: ['./templates/**/*.html', './static/src/**/*.js'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require('daisyui')],
 }
