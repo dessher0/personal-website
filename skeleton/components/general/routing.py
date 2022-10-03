b0389b9a164cda3146eeafaa22d9ca405d55aa80
Maciej <m.kula@netperformers.de>
2022-10-04 00:22:39 +0200

    Updated routes

--- skeleton/components/general/routing.py
+++ skeleton/components/general/routing.py
@@ -2,14 +2,27 @@ from flask import Blueprint, render_template, request
 from flask_mail import Mail, Message
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_httpauth import HTTPBasicAuth
 from ...config import config
-from ... import g_app
+from ... import g_app, users
 from ...apps.gpt3 import gpt3
+from werkzeug.security import check_password_hash
 
 
 views = Blueprint("views", __name__)
 mail = Mail(g_app)
 limiter = Limiter(g_app, key_func=get_remote_address)
+auth = HTTPBasicAuth()
+
+
+@auth.error_handler
+def auth_error():
+    return render_template("errors/403.html")
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
 
 
 @views.context_processor
@@ -17,7 +30,6 @@ def process():
     currentYear = config["development"].CURRENT_YEAR
 
     return dict(currentYear=currentYear)
-
 
 @views.route("/")
 def index():
@@ -30,8 +42,15 @@ def about():
 
 
 @views.route("/projects")
+@auth.login_required
 def projects():
     return render_template("general/projects.html", **locals())
+
+
+@views.route("/games")
+@auth.login_required
+def games():
+    return render_template("general/games.html", **locals())
 
 
 @views.route("/openai", methods=["GET", "POST"])
